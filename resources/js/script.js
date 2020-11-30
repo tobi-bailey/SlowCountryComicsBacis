6f81a62ad7b0e06810b4605772545cb05d53e5e4
Tobi Bailey <baileygtobi@gmail.com>
2020-11-30 12:33:37 -0500

    updated archive and button automation

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,16 +1,4 @@
 
-
-// var images = ["https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&h=350", "https://i2.wp.com/beebom.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg?resize=640%2C426", "https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350"]
-
-// var imgState = 0;
-
-// var imgTag = document.getElementById("imgClickAndChange");
-
-// imgTag.addEventListener("click", function (event) {
-//   imgState = (++imgState % images.length);
-//   event.target.src = images[imgState];
-// });
-
 var myComic = [
     "../images/comics/39.BadGrief.jpg", 
     "../images/comics/38.Jellyfish.jpg", 
@@ -49,10 +37,6 @@ var myComic = [
 ];
 
 var clickComic = document.getElementById("randomComic");
-
-// onclick = choosePic;
-
-// clickComic.addEventListener(onclick, 
 
 function choosePic() {
     var randomComic = Math.floor(Math.random() * myComic.length);
