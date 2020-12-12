c699c4694cc856488723359a807bdd2f7b6814fe
Tobi Bailey <baileygtobi@gmail.com>
2020-12-12 15:21:25 -0800

    updated media queries

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,4 @@
-
+// random comic
 var myComic = [
     "../images/comics/39.BadGrief.jpg", 
     "../images/comics/38.Jellyfish.jpg", 
@@ -43,3 +43,24 @@ function choosePic() {
     document.getElementById("randomComic").src = myComic[randomComic]; 
 };
 
+// modal
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("randomComic");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
